bd63805e9d77a997a14851e9b8a19400e9f88c7b
djdongjae <jk47wee@gmail.com>
2023-11-22 17:34:29 +0900

    ci/cd: 파일 위치 변경

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ REPOSITORY=/home/ec2-user/app/step2
 
 echo "> Build 파일 복사"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
