e6cceee8fff95813e672d778ec116581389aa265
djdongjae <jk47wee@gmail.com>
2023-12-06 20:17:17 +0900

    docs: 로그 처리 관련 코드 추가

--- src/main/java/com/groom/cookiehouse/common/advice/ControllerExceptionAdvice.java
+++ src/main/java/com/groom/cookiehouse/common/advice/ControllerExceptionAdvice.java
@@ -4,12 +4,12 @@ import com.groom.cookiehouse.common.dto.BaseResponse;
 import com.groom.cookiehouse.exception.ErrorCode;
 import com.groom.cookiehouse.exception.model.CustomException;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.exception.ConstraintViolationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.FieldError;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingRequestHeaderException;
@@ -28,6 +28,8 @@ import java.util.Objects;
 @RequiredArgsConstructor
 public class ControllerExceptionAdvice {
 
+    private static final Logger logger = LoggerFactory.getLogger(ControllerExceptionAdvice.class);
+
     /**
      * 400 BAD_REQUEST
      */
@@ -36,30 +38,35 @@ public class ControllerExceptionAdvice {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     protected BaseResponse handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
         FieldError fieldError = Objects.requireNonNull(e.getFieldError());
+        logger.error("Validation error for field {}: {}", fieldError.getField(), fieldError.getDefaultMessage());
         return BaseResponse.error(ErrorCode.VALIDATION_REQUEST_MISSING_EXCEPTION, String.format("%s. (%s)", fieldError.getDefaultMessage(), fieldError.getField()));
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MissingRequestHeaderException.class)
     protected BaseResponse handleMissingRequestHeaderException(final MissingRequestHeaderException e) {
+        logger.error("Missing Request Header: {}", e.getMessage());
         return BaseResponse.error(ErrorCode.VALIDATION_REQUEST_HEADER_MISSING_EXCEPTION, String.format("%s. (%s)", ErrorCode.VALIDATION_REQUEST_HEADER_MISSING_EXCEPTION.getMessage(), e.getHeaderName()));
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MissingServletRequestParameterException.class)
     protected BaseResponse handleMissingRequestParameterException(final MissingServletRequestParameterException e) {
+        logger.error("Missing Request Parameter: {}", e.getMessage());
         return BaseResponse.error(ErrorCode.VALIDATION_REQUEST_PARAMETER_MISSING_EXCEPTION, String.format("%s. (%s)", ErrorCode.VALIDATION_REQUEST_PARAMETER_MISSING_EXCEPTION.getMessage(), e.getParameterName()));
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     protected BaseResponse handleHttpRequestMethodNotSupportedException(final HttpRequestMethodNotSupportedException e) {
+        logger.error("Http Request Method Not Supported: {}", e.getMessage());
         return BaseResponse.error(ErrorCode.REQUEST_METHOD_VALIDATION_EXCEPTION, e.getMessage());
     }
 
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     @ResponseStatus(HttpStatus.PAYLOAD_TOO_LARGE)
     public BaseResponse fileSizeLimitExceeded(final MaxUploadSizeExceededException e) {
+        logger.error("File Size Limit Exceeded: {}", e.getMessage());
         return BaseResponse.error(ErrorCode.MAX_UPLOAD_SIZE_EXCEED_EXCEPTION, e.getMessage());
     }
 
@@ -68,7 +75,8 @@ public class ControllerExceptionAdvice {
      */
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
-    protected BaseResponse<Object> handleException(final Exception error, final HttpServletRequest request) throws IOException {
+    protected BaseResponse handleException(final Exception error, final HttpServletRequest request) throws IOException {
+        logger.error("Internal Server Error: {}", error.getMessage());
         return BaseResponse.error(ErrorCode.INTERNAL_SERVER_ERROR);
     }
     /**
@@ -76,6 +84,7 @@ public class ControllerExceptionAdvice {
      */
     @ExceptionHandler(CustomException.class)
     protected ResponseEntity<BaseResponse> handleGroomException(CustomException e) {
+        logger.error("CustomException: {}", e);
         return ResponseEntity.status(e.getHttpStatus())
                 .body(BaseResponse.error(e.getErrorCode(), e.getMessage()));
     }
