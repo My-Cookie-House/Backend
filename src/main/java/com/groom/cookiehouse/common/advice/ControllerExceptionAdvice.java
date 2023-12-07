82534c14c88583f88eb79004da87e4dfb8ed4101
djdongjae <jk47wee@gmail.com>
2023-12-07 15:35:39 +0900

    refactor: 로그 출력 형식 변경

--- src/main/java/com/groom/cookiehouse/common/advice/ControllerExceptionAdvice.java
+++ src/main/java/com/groom/cookiehouse/common/advice/ControllerExceptionAdvice.java
@@ -39,39 +39,34 @@ public class ControllerExceptionAdvice {
     protected BaseResponse handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
         FieldError fieldError = Objects.requireNonNull(e.getFieldError());
         logger.error("Validation error for field {}: {}", fieldError.getField(), fieldError.getDefaultMessage());
-        e.printStackTrace();
         return BaseResponse.error(ErrorCode.VALIDATION_REQUEST_MISSING_EXCEPTION, String.format("%s. (%s)", fieldError.getDefaultMessage(), fieldError.getField()));
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MissingRequestHeaderException.class)
     protected BaseResponse handleMissingRequestHeaderException(final MissingRequestHeaderException e) {
-        logger.error("Missing Request Header: {}", e.getMessage());
-        e.printStackTrace();
+        logger.error("Missing Request Header: {}", e.getMessage(), e);
         return BaseResponse.error(ErrorCode.VALIDATION_REQUEST_HEADER_MISSING_EXCEPTION, String.format("%s. (%s)", ErrorCode.VALIDATION_REQUEST_HEADER_MISSING_EXCEPTION.getMessage(), e.getHeaderName()));
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MissingServletRequestParameterException.class)
     protected BaseResponse handleMissingRequestParameterException(final MissingServletRequestParameterException e) {
-        logger.error("Missing Request Parameter: {}", e.getMessage());
-        e.printStackTrace();
+        logger.error("Missing Request Parameter: {}", e.getMessage(), e);
         return BaseResponse.error(ErrorCode.VALIDATION_REQUEST_PARAMETER_MISSING_EXCEPTION, String.format("%s. (%s)", ErrorCode.VALIDATION_REQUEST_PARAMETER_MISSING_EXCEPTION.getMessage(), e.getParameterName()));
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     protected BaseResponse handleHttpRequestMethodNotSupportedException(final HttpRequestMethodNotSupportedException e) {
-        logger.error("Http Request Method Not Supported: {}", e.getMessage());
-        e.printStackTrace();
+        logger.error("Http Request Method Not Supported: {}", e.getMessage(), e);
         return BaseResponse.error(ErrorCode.REQUEST_METHOD_VALIDATION_EXCEPTION, e.getMessage());
     }
 
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     @ResponseStatus(HttpStatus.PAYLOAD_TOO_LARGE)
     public BaseResponse fileSizeLimitExceeded(final MaxUploadSizeExceededException e) {
-        logger.error("File Size Limit Exceeded: {}", e.getMessage());
-        e.printStackTrace();
+        logger.error("File Size Limit Exceeded: {}", e.getMessage(), e);
         return BaseResponse.error(ErrorCode.MAX_UPLOAD_SIZE_EXCEED_EXCEPTION, e.getMessage());
     }
 
@@ -80,9 +75,8 @@ public class ControllerExceptionAdvice {
      */
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
-    protected BaseResponse handleException(final Exception error, final HttpServletRequest request) throws IOException {
-        logger.error("Internal Server Error: {}", error.getMessage());
-        error.printStackTrace();
+    protected BaseResponse handleException(final Exception e, final HttpServletRequest request) throws IOException {
+        logger.error("Internal Server Error: {}", e.getMessage(), e);
         return BaseResponse.error(ErrorCode.INTERNAL_SERVER_ERROR);
     }
     /**
@@ -90,8 +84,7 @@ public class ControllerExceptionAdvice {
      */
     @ExceptionHandler(CustomException.class)
     protected ResponseEntity<BaseResponse> handleGroomException(CustomException e) {
-        logger.error("CustomException: {}", e.getMessage());
-        e.printStackTrace();
+        logger.error("CustomException: {}", e.getMessage(), e);
         return ResponseEntity.status(e.getHttpStatus())
                 .body(BaseResponse.error(e.getErrorCode(), e.getMessage()));
     }
